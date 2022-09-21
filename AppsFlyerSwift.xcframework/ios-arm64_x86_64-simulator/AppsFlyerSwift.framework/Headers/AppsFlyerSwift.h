5006e9f13f69c193aa297f1f22b39f75b2265617
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2022-09-21 15:46:29 +0300

    Initial commit

---
+++ AppsFlyerSwift.xcframework/ios-arm64_x86_64-simulator/AppsFlyerSwift.framework/Headers/AppsFlyerSwift.h
@@ -0,0 +1,18 @@
+//
+//  AppsFlyerSwift.h
+//  AppsFlyerSwift
+//
+//  Created by ivan.obodovskyi on 13.09.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppsFlyerSwift.
+FOUNDATION_EXPORT double AppsFlyerSwiftVersionNumber;
+
+//! Project version string for AppsFlyerSwift.
+FOUNDATION_EXPORT const unsigned char AppsFlyerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppsFlyerSwift/PublicHeader.h>
+
+
